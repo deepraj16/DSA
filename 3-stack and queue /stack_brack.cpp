c9eecd3eeb86305736250c80d9f2b71d6210f8d6
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-04-10 19:45:18 +0530

    Create stack_brack.cpp

---
+++ 3-stack and queue /stack_brack.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isreduant(string a,stack<char>s){
+    for(int i=0;i<a.length();i++){
+        char ch=a[i];
+        if(ch=='(' || ch=='+' || ch=='-' || ch=='/' || ch=='*'){
+                s.push(ch);
+        }
+        else{
+            if(ch==')'){
+                bool isredunt=true;
+                    while(s.top()!='(')
+                    {
+                        char top=s.top();
+                        if( top=='+' || top=='-' || top=='/' || top=='*'){
+                            isredunt=false;
+                           return false;
+                    }
+                    s.pop();
+                    }
+                    if(isredunt==true){
+                        return true;
+                        s.pop();
+                    }
+                }
+        }
+
+    }
+    return false;
+}
+int main(){
+    string a="((a+c)*b)";
+    stack<char>s; 
+    cout<<isreduant(a,s);
+    return 0;
+}
