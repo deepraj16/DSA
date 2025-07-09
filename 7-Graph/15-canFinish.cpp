358998066f3dc562630c6446f49359cbee12708a
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-07-09 21:03:09 +0530

    Create 15-canFinish.cpp

---
+++ 7-Graph/15-canFinish.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+   
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegre(numCourses,0);
+        for(auto i:prerequisites){
+            int u=i[0];
+            int v=i[1]; 
+            adj[v].push_back(u);
+            indegre[u]++;
+        }
+        // vector<int>visted(numCourses,0);
+       //  vector<int>indegre(numCourses,0); 
+        
+        queue<int>q; 
+        for(int i=0;i<numCourses;i++){
+            if(indegre[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int>ans; 
+        while(!q.empty()){
+            int node=q.front(); 
+            q.pop(); 
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegre[it]--; 
+                if(indegre[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+
+        //by khans method 
+        
+        return ans.size()==numCourses;
+    }
+};
