20e3210b5d46ffc49aec6893e1774a7089a44fb9
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-07-24 21:13:26 +0530

    Create 29-swimInWate.cpp

---
+++ 7-Graph/29-swimInWate.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+
+        pq.push({grid[0][0], 0, 0});
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!pq.empty()) {
+            auto [time, x, y] = pq.top(); pq.pop();
+
+            if (visited[x][y]) continue;
+            visited[x][y] = true;
+
+            if (x == n - 1 && y == n - 1)
+                return time;
+
+            for (auto [dx, dy] : directions) {
+                int nx = x + dx, ny = y + dy;
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny]) {
+                    pq.push({max(time, grid[nx][ny]), nx, ny});
+                }
+            }
+        }
+
+        return -1; 
+    }
+};
