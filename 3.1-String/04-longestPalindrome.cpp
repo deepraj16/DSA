aed67837b5384fe5e6e61a24340da0509c0e0760
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-09-29 10:26:52 +0530

    Create 04-longestPalindrome.cpp

---
+++ 3.1-String/04-longestPalindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string Expand_around(string &s,int left,int right){
+        while(left>=0 and right<s.length() and s[left]==s[right]){
+            left--; 
+            right++;
+        }
+        return s.substr(left+1,right-left-1);
+
+
+    }
+    string longestPalindrome(string s) {
+        string longest=""; 
+        for(int i=0;i<s.length();i++){
+
+            string odd=Expand_around(s,i,i); 
+
+            string even=Expand_around(s,i,i+1); 
+
+
+            if(odd.length()>longest.length()){
+                longest=odd;
+            }
+            if(even.length()>longest.length()){
+                longest=even;
+            }
+        }
+        return longest;
+        
+    }
+};
