5498c9d3fc0c8349f452b4195e3c2ad8fc0cfb57
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-10-09 08:31:55 +0530

    Create 13-myAtoi.cpp

---
+++ 3.1-String/13-myAtoi.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int flag=0;
+        long long ans=0; 
+        int countsub=0;
+        string symoble="";
+        bool sy=false;
+        for(int i=0;i<s.size();i++){
+
+            if((s[i]=='+' || s[i]=='-' )){
+                if(sy==true){
+                    break;
+                }
+                symoble=s[i];
+                sy=true;
+            }
+            if(isalpha(s[i]) || s[i]=='.' || (sy && s[i]==' ')){
+                break;
+            }
+             while(isdigit(s[i])){
+             int curr=s[i] - '0' ;
+             flag=1;
+             if(ans> (INT_MAX-curr)/10 ){
+                if(symoble[0]=='-') return INT_MIN;
+                return INT_MAX;
+             }
+             ans=ans*10+curr;
+             i++;
+             }
+
+
+             if(flag==1){
+                break;
+             }
+             
+        }
+        
+
+         if(symoble[0]=='-') return ans*-1;
+        return (int)ans;
+    }
+};
