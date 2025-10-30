af1fd89a51e8927e6e78249ae8dd3902e7f93cb7
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-10-30 20:01:02 +0530

    Create 21-isPalindrome.cpp

---
+++ 3.1-String/21-isPalindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+
+        for(auto it :s){
+            if (isalnum(it)) {
+               char ch = tolower(it);
+                ans+=ch;
+            }
+        }
+        cout<<ans;
+        int n=ans.size();
+        
+        for(int i=0;i<ans.size()/2;i++){
+            if(ans[i] != ans[n-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
