9ba446c0605f04d0118e38e287fbd99e4ae5fd33
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-10-31 15:07:23 +0530

    Update 21-isPalindrome.cpp

--- 3.1-String/21-isPalindrome.cpp
+++ 3.1-String/21-isPalindrome.cpp
@@ -3,15 +3,13 @@ public:
     bool isPalindrome(string s) {
         string ans="";
 
-        for(auto it :s){
+        for(char it :s){
             if (isalnum(it)) {
                char ch = tolower(it);
                 ans+=ch;
             }
         }
-        cout<<ans;
         int n=ans.size();
-        
         for(int i=0;i<ans.size()/2;i++){
             if(ans[i] != ans[n-i-1]){
                 return false;
